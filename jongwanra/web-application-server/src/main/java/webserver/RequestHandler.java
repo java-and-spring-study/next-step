b0099fbe33bea6ea162562fd0263b2a389d105a6
jongwanra <najongsi@gmail.com>
2024-02-14 17:43:56 +0900

    힌트 확인 이후에 리팩토링

--- jongwanra/web-application-server/src/main/java/webserver/RequestHandler.java
+++ jongwanra/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -2,12 +2,14 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
     // private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,9 +35,8 @@ public class RequestHandler extends Thread {
             DataOutputStream dos = new DataOutputStream(out);
 
             final String methodAndHost = bufferedReader.readLine();
-
             if(isStaticFile(methodAndHost)) {
-                handleStaticFile(dos, methodAndHost.split(" ")[1]);
+                handleStaticFileV2(dos, extractFilePath(methodAndHost));
                 return;
             }
 
@@ -51,11 +52,15 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private String extractFilePath(String methodAndHost) {
+        return methodAndHost.split(" ")[1];
+    }
+
     private boolean isStaticFile(final String value) {
         return value.contains(".html");
     }
 
-    private void handleStaticFile(DataOutputStream dos, final String filePath) throws IOException {
+    private void handleStaticFileV1(DataOutputStream dos, final String filePath) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("webapp" + filePath));
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -66,6 +71,16 @@ public class RequestHandler extends Thread {
         reader.close();
 
         final byte[] body = stringBuilder.toString().getBytes();
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+
+    }
+
+    /**
+     * 책의 힌트를 보고 재구성
+     */
+    private void handleStaticFileV2(DataOutputStream dos, final String filePath) throws IOException {
+        byte[] body = Files.readAllBytes(new File("webapp" + filePath).toPath());
         response200Header(dos, body.length);
         responseBody(dos, body);
 
