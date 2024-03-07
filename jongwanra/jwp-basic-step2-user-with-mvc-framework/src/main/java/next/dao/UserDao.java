7575b5136173904097ccde1ec2379e877372d2a5
jongwanra <najongsi@gmail.com>
2024-03-07 22:28:08 +0900

    jongwanra: findAll() method 구현

--- jongwanra/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/UserDao.java
+++ jongwanra/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/UserDao.java
@@ -40,8 +40,33 @@ public class UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<User> users = new ArrayList<>();
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT * FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                User foundUser = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                    rs.getString("email"));
+                users.add(foundUser);
+            }
+            return users;
+        }finally {
+            if(rs != null) {
+                rs.close();
+            }
+            if(pstmt != null) {
+                pstmt.close();
+            }
+            if(con != null) {
+                con.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws SQLException {
