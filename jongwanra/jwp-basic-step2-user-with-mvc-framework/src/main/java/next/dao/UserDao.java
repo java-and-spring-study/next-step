ee5645edf2cba68d0ce60e6efc710badcb762e4a
jongwanra <najongsi@gmail.com>
2024-03-07 22:39:16 +0900

    jongwanra: update() method 구현

--- jongwanra/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/UserDao.java
+++ jongwanra/jwp-basic-step2-user-with-mvc-framework/src/main/java/next/dao/UserDao.java
@@ -36,7 +36,30 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS SET password=?, email=?, name=? WHERE userId=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getUserId());
+
+            pstmt.executeUpdate();
+        }finally {
+            if(con != null) {
+                con.close();
+            }
+            if(pstmt != null) {
+                pstmt.close();
+            }
+            if(rs != null) {
+                rs.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException {
