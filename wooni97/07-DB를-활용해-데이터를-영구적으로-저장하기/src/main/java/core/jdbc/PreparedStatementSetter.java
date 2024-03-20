15f5df8321beb21a85929fe55bf9b21f06de8cbc
wooni97 <jhw970714@gmail.com>
2024-03-21 07:59:40 +0900

    7장 구현

---
+++ wooni97/07-DB를-활용해-데이터를-영구적으로-저장하기/src/main/java/core/jdbc/PreparedStatementSetter.java
@@ -0,0 +1,8 @@
+package core.jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+@FunctionalInterface
+public interface PreparedStatementSetter {
+    void values(PreparedStatement pstmt) throws SQLException;
+}
