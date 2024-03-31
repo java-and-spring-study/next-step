c3529e9b741ec5996b6ad8f06ca69a5b4073260b
jongwanra <najongsi@gmail.com>
2024-03-31 18:10:51 +0900

    jongwanra: self-check 6

--- jongwanra/jwp-basic-step7-self-check/src/main/java/core/web/filter/CharacterEncodingFilter.java
+++ jongwanra/jwp-basic-step7-self-check/src/main/java/core/web/filter/CharacterEncodingFilter.java
@@ -8,9 +8,16 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+
+/**
+ * Todo
+ * utf-8로 인코딩을 해주지 않으면 왜 꺠질까?
+ */
+@WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
-    private static final String DEFAULT_ENCODING = "UTF-8";
+    private static final String UTF_8 = "UTF-8";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -19,8 +26,8 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        request.setCharacterEncoding(DEFAULT_ENCODING);
-        response.setCharacterEncoding(DEFAULT_ENCODING);
+        request.setCharacterEncoding(UTF_8);
+        response.setCharacterEncoding(UTF_8);
         chain.doFilter(request, response);
 
     }
