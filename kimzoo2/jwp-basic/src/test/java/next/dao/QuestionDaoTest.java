5972fa1549790a2c2ea083600fa2d19e0aa103fa
kimzoo2 <kimzoo1207@gmail.com>
2024-04-08 21:40:13 +0900

    생성자 파라미터 추가

--- kimzoo2/jwp-basic/src/test/java/next/dao/QuestionDaoTest.java
+++ kimzoo2/jwp-basic/src/test/java/next/dao/QuestionDaoTest.java
@@ -9,22 +9,26 @@ import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import core.jdbc.ConnectionManager;
+import core.jdbc.JdbcTemplate;
 import next.model.Question;
 
 public class QuestionDaoTest {
     private static final Logger log = LoggerFactory.getLogger(QuestionDaoTest.class);
+
+    private JdbcTemplate jdbcTemplate;
     
     @Before
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+        jdbcTemplate = new JdbcTemplate();
     }
     
     @Test
     public void crud() {
         Question question = new Question("writer", "title", "contents");
-        QuestionDao questionDao = new QuestionDao();
+        QuestionDao questionDao = new QuestionDao(jdbcTemplate);
         Question savedQuestion = questionDao.insert(question);
         log.debug("question : {}", savedQuestion);
     }
