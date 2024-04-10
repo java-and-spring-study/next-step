af7d4411616ab7fb0b5a14bc2e51c76b06419d2c
kimzoo2 <kimzoo1207@gmail.com>
2024-04-10 14:56:14 +0900

    ControllerScanner prefix 파라미터 전달 받도록 변경

--- kimzoo2/jwp-basic/src/main/java/core/annotation/ControllerScanner.java
+++ kimzoo2/jwp-basic/src/main/java/core/annotation/ControllerScanner.java
@@ -9,8 +9,14 @@ import org.reflections.Reflections;
 // 각 클래스의 인스턴스 생성을 담당한다.
 public class ControllerScanner {
 
+	private Object[] prefix;
+
+	public ControllerScanner(Object[] prefix) {
+		this.prefix = prefix;
+	}
+
 	public Map<Class<?>, Object> getControllers() throws InstantiationException, IllegalAccessException {
-		Reflections reflections = new Reflections("core.nmvc");
+		Reflections reflections = new Reflections(prefix[0]);
 		//주어진 어노테이션을 구현하는 모든 클래스와 인터페이스를 가져올 수 있다.
 		return instantiateControllers(reflections.getTypesAnnotatedWith(Controller.class));
 	}
